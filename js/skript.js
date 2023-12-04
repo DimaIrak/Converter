210fc949b2eaf3dda8f4bd066fb3a77fe7f0e15c
Dmytro Artiuk <dimairak88@wp.pl>
2023-12-04 23:40:21 +0100

    adde functions

--- js/skript.js
+++ js/skript.js
@@ -1,14 +1,47 @@
 console.log("Witam wszystkich!");
 
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
+{
+    const calculateResult = (amount, currency) => {
+        const rateGBP = 4.93;
+        const rateUSD = 3.90;
+        const rateEUR = 4.42;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+        switch (currency) {
+            case "GBP":
+                return amount / currency;
 
-    let amount = amountElement.value;
-    let result = amount / 4.3577;
-    resultElement.innerText = result.toFixed(2);
-})
-    ;
+            case "USD":
+                return amount / currency;
+
+            case "EUR":
+                return amount / currency;
+        }
+    };
+    const updateResultText = (amount, result, currency) => {
+
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = '<strong>${result.toFixed(2)} ${currency}</strong>';
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+
+        updateResultText(amount, result, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
